ed29789e6a54317f5cbce38b2478263c52574c11
Alex Antonyuk <alex@antonyuk.me>
2014-11-03 11:34:37 +0200

    Initial Commit

---
+++ AddressBookTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AddressBookTest
+//
+//  Created by Alex Antonyuk on 11/3/14.
+//  Copyright (c) 2014 Alex Antonyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
